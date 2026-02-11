68da655fe9b830ff6bdcbec7588711bdb84784db
infonishant1 <infonishantgaurav2@gmail.com>
2026-02-11 23:32:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/sog14-edossier/',
   define: {
     // This allows the existing code to use process.env.API_KEY in the browser
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
